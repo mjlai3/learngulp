444d1871ae266ed48ecdb194210b8f0dd2407c8e
Michael Lai <michaeljin.lai@gmail.com>
2017-05-10 14:45:45 +1000

    Add sass and change folder structure for css and js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,22 +7,23 @@ import uglify from "gulp-uglify";
 import source from "vinyl-source-stream";
 import buffer from "vinyl-buffer";
 import pug from "gulp-pug";
+import sass from "gulp-sass";
 
 gulp.task('scripts', () => {
 
 	// Generate non minified
 	browserify({
-		entries: './lib/main.js',
+		entries: './lib/_scripts/main.js',
 		debug: true
 	})
 	.transform("babelify", { presets: ["es2015"] })
 	.bundle()
 	.pipe(source('main.js')) // **
-	.pipe(gulp.dest('./dist'));
+	.pipe(gulp.dest('./dist/scripts'));
 
 	// Generate minified
 	browserify({
-		entries: './lib/main.js',
+		entries: './lib/_scripts/main.js',
 		debug: true
 	})
 	.transform("babelify", { presets: ["es2015"] })
@@ -30,9 +31,15 @@ gulp.task('scripts', () => {
 	.pipe(source('main.min.js')) // **
 	.pipe(buffer())
 	.pipe(uglify())
-	.pipe(gulp.dest('./dist'));
+	.pipe(gulp.dest('./dist/scripts'));
 
 	// ** bundle() directly to gulp.dest won’t work when you need to pipe with other gulp plugins like uglify or gulp.dist. This is because browserify.bundle() return a text stream where as gulp works using vinyl stream. In order to browserify to work with other plugins you need to use vinyl-source-stream. (http://blog.revathskumar.com/2016/02/browserify-with-gulp.html)
+});
+ 
+gulp.task('sass', () => {
+  gulp.src('./lib/_styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/styles'));
 });
  
 gulp.task('pug', () => {
@@ -42,6 +49,7 @@ gulp.task('pug', () => {
 });
 
 gulp.task('serve', [
+	'sass',
 	'pug',
 	'scripts',
 	'watch'
@@ -49,10 +57,12 @@ gulp.task('serve', [
 
 gulp.task('watch', function () {
     gulp.watch([
-		'./lib/*.js',
-		'./lib/*.pug'
+		'./lib/**/*.scss',
+		'./lib/*.pug',
+		'./lib/**/*.js'
 	],
 	[
+		'sass',
 		'pug',
 		'scripts'
 	]);
