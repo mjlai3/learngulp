135aef0f12603489e2e6e3f9401168f716a58ecf
Michael Lai <michaeljin.lai@gmail.com>
2017-05-10 12:45:59 +1000

    Use babelify to enable ES6 in source files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,7 +6,7 @@ import browserify from "browserify";
 import uglify from "gulp-uglify";
 import source from "vinyl-source-stream";
 import buffer from "vinyl-buffer";
- 
+
 gulp.task('scripts', () => {
 
 	// Generate non minified
@@ -14,6 +14,7 @@ gulp.task('scripts', () => {
 		entries: './lib/main.js',
 		debug: true
 	})
+	.transform("babelify", { presets: ["es2015"] })
 	.bundle()
 	.pipe(source('main.js')) // **
 	.pipe(gulp.dest('./dist'));
@@ -23,13 +24,14 @@ gulp.task('scripts', () => {
 		entries: './lib/main.js',
 		debug: true
 	})
+	.transform("babelify", { presets: ["es2015"] })
 	.bundle()
 	.pipe(source('main.min.js')) // **
 	.pipe(buffer())
 	.pipe(uglify())
 	.pipe(gulp.dest('./dist'));
 
-	// ** A basic implementation can be done as above. But this won’t work when you need to pipe with other gulp plugins like uglify or gulp.dist. This is because browserify.bundle() return a text stream where as gulp works using vinyl stream. In order to browserify to work with other plugins you need to use vinyl-source-stream.
+	// ** bundle() directly to gulp.dest won’t work when you need to pipe with other gulp plugins like uglify or gulp.dist. This is because browserify.bundle() return a text stream where as gulp works using vinyl stream. In order to browserify to work with other plugins you need to use vinyl-source-stream. (http://blog.revathskumar.com/2016/02/browserify-with-gulp.html)
 });
  
 gulp.task('html', () => {
