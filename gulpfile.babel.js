8041da0b0dcd4657ea1010fd921b68e4a488e5b8
Michael Lai <michaeljin.lai@gmail.com>
2017-05-10 14:22:39 +1000

    Add pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ import browserify from "browserify";
 import uglify from "gulp-uglify";
 import source from "vinyl-source-stream";
 import buffer from "vinyl-buffer";
+import pug from "gulp-pug";
 
 gulp.task('scripts', () => {
 
@@ -34,13 +35,14 @@ gulp.task('scripts', () => {
 	// ** bundle() directly to gulp.dest won’t work when you need to pipe with other gulp plugins like uglify or gulp.dist. This is because browserify.bundle() return a text stream where as gulp works using vinyl stream. In order to browserify to work with other plugins you need to use vinyl-source-stream. (http://blog.revathskumar.com/2016/02/browserify-with-gulp.html)
 });
  
-gulp.task('html', () => {
-  gulp.src(['./lib/index.html']) // source
-    .pipe(gulp.dest('./dist/')); // destination
+gulp.task('pug', () => {
+  gulp.src('./lib/*.pug')
+  	.pipe(pug())
+  	.pipe(gulp.dest('./dist'))
 });
 
 gulp.task('serve', [
-	'html',
+	'pug',
 	'scripts',
 	'watch'
 ]);
@@ -48,10 +50,10 @@ gulp.task('serve', [
 gulp.task('watch', function () {
     gulp.watch([
 		'./lib/*.js',
-		'./lib/*.html'
+		'./lib/*.pug'
 	],
 	[
-		'html',
+		'pug',
 		'scripts'
 	]);
 });
