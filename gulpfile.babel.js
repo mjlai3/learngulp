40a4928cdbdc750600cfa8e31ce1a0c5c9540182
Michael Lai <michaeljin.lai@gmail.com>
2017-05-10 15:23:26 +1000

    Add browsersync, comment out js uglify and separate gulp watch for each type of file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,14 @@
 import gulp from 'gulp';
 import concat from 'gulp-concat';
 import browserify from "browserify";
+import browserSyncLib from 'browser-sync';
 import uglify from "gulp-uglify";
 import source from "vinyl-source-stream";
 import buffer from "vinyl-buffer";
 import pug from "gulp-pug";
 import sass from "gulp-sass";
+
+let browserSync = browserSyncLib.create();
 
 gulp.task('scripts', () => {
 
@@ -22,28 +25,37 @@ gulp.task('scripts', () => {
 	.pipe(gulp.dest('./dist/scripts'));
 
 	// Generate minified
-	browserify({
-		entries: './lib/_scripts/main.js',
-		debug: true
-	})
-	.transform("babelify", { presets: ["es2015"] })
-	.bundle()
-	.pipe(source('main.min.js')) // **
-	.pipe(buffer())
-	.pipe(uglify())
-	.pipe(gulp.dest('./dist/scripts'));
+	// browserify({
+	// 	entries: './lib/_scripts/main.js',
+	// 	debug: true
+	// })
+	// .transform("babelify", { presets: ["es2015"] })
+	// .bundle()
+	// .pipe(source('main.min.js')) // **
+	// .pipe(buffer())
+	// .pipe(uglify())
+	// .pipe(gulp.dest('./dist/scripts'));
 
 	// ** bundle() directly to gulp.dest won’t work when you need to pipe with other gulp plugins like uglify or gulp.dist. This is because browserify.bundle() return a text stream where as gulp works using vinyl stream. In order to browserify to work with other plugins you need to use vinyl-source-stream. (http://blog.revathskumar.com/2016/02/browserify-with-gulp.html)
 });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
  
 gulp.task('sass', () => {
-  gulp.src('./lib/_styles/*.scss')
+  return gulp.src('./lib/_styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/styles'));
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(browserSync.stream());
 });
  
 gulp.task('pug', () => {
-  gulp.src('./lib/*.pug')
+  return gulp.src('./lib/*.pug')
   	.pipe(pug())
   	.pipe(gulp.dest('./dist'))
 });
@@ -52,18 +64,18 @@ gulp.task('serve', [
 	'sass',
 	'pug',
 	'scripts',
+	'browser-sync',
 	'watch'
 ]);
 
 gulp.task('watch', function () {
-    gulp.watch([
-		'./lib/**/*.scss',
-		'./lib/*.pug',
-		'./lib/**/*.js'
-	],
-	[
-		'sass',
-		'pug',
-		'scripts'
-	]);
+    gulp.watch(['./lib/*.pug'],['pug']);
+    gulp.watch(['./lib/**/*.js'], ['scripts']);
+    gulp.watch(['./lib/**/*.scss'],['sass']);
+
+	// Watch dist for BrowserSync
+	gulp.watch([
+		"./dist/*.html",
+		"./dist/scripts/*.js"
+	]).on('change', browserSync.reload);
 });
